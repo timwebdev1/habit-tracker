b8bf25829d1965f5a7a9fcc5aa37cafc4b9da69f
Timothy McDermott <162518123+timwebdev1@users.noreply.github.com>
2025-05-05 05:50:07 -0500

    feat: built habit creation funcitonality and delete & completed to habit cards

---
+++ app.js
@@ -0,0 +1,68 @@
+const habitList = [];
+
+function loadHabits() {
+  const storedHabits = localStorage.getItem("habits");
+  if (storedHabits) {
+    const parsedHabits = JSON.parse(storedHabits);
+    habitList.push(...parsedHabits);
+  }
+  renderHabits();
+}
+
+function addHabit() {
+  const input = document.getElementById("habit");
+  const value = input.value.trim();
+
+  if (!value) return;
+
+  const habit = {
+    id: Date.now(),
+    name: value,
+    completed: false,
+  };
+
+  habitList.push(habit);
+  saveHabits();
+  renderHabits();
+  input.value = "";
+}
+
+function saveHabits() {
+  return localStorage.setItem("habits", JSON.stringify(habitList));
+}
+
+document.getElementById("habit-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  addHabit();
+});
+
+function renderHabits() {
+  const habitContainer = document.getElementById('habit-list');
+  habitContainer.innerHTML = '';
+
+  for(const habit of habitList) {
+    const card = document.createElement('div');
+    card.classList.add('p-4', 'rounded-lg', 'shadow', 'bg-white', 'dark:bg-slate-800');
+    card.setAttribute('data-id', habit.id);
+
+    const title = document.createElement('h3');
+    title.textContent = habit.name;
+    if (habit.completed) {
+      title.classList.add('line-through', 'text-gray-400');
+    } else {
+      title.classList.add('font-semibold', 'text-gray-800');
+    }
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = habit.completed;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+
+    card.appendChild(title);
+    card.appendChild(checkbox);
+    card.appendChild(deleteBtn);
+
+    habitContainer.appendChild(card);
+  }
+}
